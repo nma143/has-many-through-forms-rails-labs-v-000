0d66ed253e0a70bc88c1cc27526cb4abcea91119
Nikki A <nikki.apostolakis@gmail.com>
2019-01-31 03:19:44 +0000

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,16 +6,12 @@ class Comment < ActiveRecord::Base
     self.user  ? self.users.username : nil
   end
 
-  # def user_attributes=(user_hash)
-  #
-  #     # Do not create a user if it doens't have a username
-  #     if user_hash[:username].present?
-  #       user = User.find_or_create_by(username: user_hash[:username])
-  #       #make the association between this user and this comment by adding a
-  #       # row to the join table post_categories
-  #         self.post_categories.build(:category => category)
-  #       end
-  #     end
-  # end
+  def user_attributes=(user_hash)
+    # Do not create a user if it doens't have a username
+    if user_hash[:username].present?
+      user = User.find_or_create_by(username: user_hash[:username])
+      self.user_id=user.id
+    end
+  end
 
 end
