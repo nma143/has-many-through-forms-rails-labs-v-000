46fc2aa1eb5c6561e1a10ca1b3785d7f10dd2465
Nikki A <nikki.apostolakis@gmail.com>
2019-01-31 03:16:05 +0000

    Automatically backed up by Learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    raise params.inspect
+    # raise params.inspect
     comment = Comment.create(comment_params)
     redirect_to comment.post
   end
