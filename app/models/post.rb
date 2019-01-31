898271426cd68390cce43a451ebf6a9313288301
Nikki A <nikki.apostolakis@gmail.com>
2019-01-31 01:40:25 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,25 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
 
 
+  # def user_username
+  #   self.user ? self.user.username : nil
+  # end
+
+  def categories_attributes=(categories_hash)
+    categories_hash.each do |i, category_attributes|
+      #find or create the category regardless of whether this post has it
+      # Do not create a category if it doens't have a name
+      if category_attributes[:name].present?
+
+        category = Category.find_or_create_by(name: category_attributes[:name])
+        #make the association between this category and this post by adding a
+        # row to the join table post_categories
+        # Also, don't add a category to a post if it already has it
+        if !self.categories.include?(category)
+          self.post_categories.build(:category => category)
+        end
+      end
+    end
+  end
+
 end
